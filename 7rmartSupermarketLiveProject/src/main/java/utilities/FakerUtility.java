332f6a585eeba66af0af51c9f19ede8af5d5bf55
amrit <amrit@192.168.1.39>
2024-09-05 08:10:24 +0530

    Added Wait in Wait Utility, added dependencies in pom.xml,FakerUtility.java

---
+++ 7rmartSupermarketLiveProject/src/main/java/utilities/FakerUtility.java
@@ -0,0 +1,34 @@
+package utilities;
+
+import com.github.javafaker.Address;
+import com.github.javafaker.Faker;
+
+//to give fake data in our script
+public class FakerUtility 
+{
+	public String getFakeFirstName() 
+	{
+
+	Faker faker = new Faker();
+
+	Address address = faker.address();
+
+	String text = address.firstName();
+
+	return text;
+
+	}
+
+public String getFakeLastName() {
+
+	Faker faker = new Faker();
+
+	Address address = faker.address();
+
+	String text = address.lastName();
+
+	return text;
+
+}
+
+}
--- 7rmartSupermarketLiveProject/src/main/java/utilities/WaitUtility.java
+++ 7rmartSupermarketLiveProject/src/main/java/utilities/WaitUtility.java
@@ -16,13 +16,23 @@ public class WaitUtility
 		}
 		public void waitforSubCategory(WebElement element1)
 		{
-			WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(10));
+			WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(25));
 			wait.until(ExpectedConditions.elementToBeClickable(element1));
 		}
 		public void Wait(WebElement element2)
 		{
 			WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(5));
 			wait.until(ExpectedConditions.elementToBeClickable(element2));
+		}
+		public void wait(WebElement element3)
+		{
+			WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(20));
+			wait.until(ExpectedConditions.elementToBeClickable(element3));
+		}
+		public void waiting(WebElement element4)
+		{
+			WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(15));
+			wait.until(ExpectedConditions.elementToBeClickable(element4));
 		}
 	}
 	
